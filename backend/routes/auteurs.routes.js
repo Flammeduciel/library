19ace45b1de14fd6f1c6e895fc56c4f07c7b8d20
Flamme WASS <flammewassangou@gmail.com>
2026-09-10 19:37:18 +0100

    feat(middleware): integration validation sur toutes les routes CRUD

--- backend/routes/auteurs.routes.js
+++ backend/routes/auteurs.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/auteurs.controller');
+const { validateAuteur } = require('../middlewares/validation');
 
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getById);
-router.post('/', ctrl.create);
-router.put('/:id', ctrl.update);
+router.post('/', validateAuteur, ctrl.create);
+router.put('/:id', validateAuteur, ctrl.update);
 router.delete('/:id', ctrl.remove);
 
 module.exports = router;
--- backend/routes/livres.routes.js
+++ backend/routes/livres.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/livres.controller');
+const { validateLivre } = require('../middlewares/validation');
 
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getById);
-router.post('/', ctrl.create);
-router.put('/:id', ctrl.update);
+router.post('/', validateLivre, ctrl.create);
+router.put('/:id', validateLivre, ctrl.update);
 router.delete('/:id', ctrl.remove);
 
 module.exports = router;
